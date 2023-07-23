f1170d5966f1927dadf99bb65e8db3faf42a72d3
Taylor Lea <SyntacticStructures@users.noreply.github.com>
2023-07-23 15:18:59 -0700

    I still don't get what's wrong

--- main.tf
+++ main.tf
@@ -21,6 +21,7 @@ data "aws_vpc" "default" {
 resource "aws_instance" "blog" {
   ami           = data.aws_ami.app_ami.id
   instance_type = var.instance_type
+
   vpc_security_group_ids  = [aws_security_group.blog.id]
 
   tags = {
@@ -35,11 +36,11 @@ resource "aws_security_group" "blog" {
   vpc_id = data.aws_vpc.default.id
 }
 
-resource "aws_security_group_rule" "blog_http_in" {
-  type = "ingress"
-  from_port = 80
-  to_port = 80
-  protocol = "tcp"
+resource "aws_security_group_rule" "blog_https_in" {
+  type        = "ingress"
+  from_port   = 443
+  to_port     = 443
+  protocol    = "tcp"
   cidr_blocks = ["0.0.0.0/0"]
 
   security_group_id = "aws_security_group.blog.id"
